a304cdbc4a8131035522b8ec8adcbcfd32fd8a79
shayanalvi <shayanalvi6@gmail.com>
2023-04-12 17:04:59 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
     void main() {
   runApp(MyApp());
@@ -10,13 +11,7 @@ import 'package:flutter/material.dart';
       @override
       Widget build(BuildContext context) {
         return MaterialApp(
-          home: Material(
-            child: Center(
-              child: Container(
-                child: Text("welcome"),
-              ),
-            ),
-          ),
+          home: HomePage(),
         );
       }
     }
