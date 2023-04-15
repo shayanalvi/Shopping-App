6b3c092eab11e76a5d7148af8f2b7ee15fdddb17
shayanalvi <shayanalvi6@gmail.com>
2023-04-15 13:46:20 +0500

    update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,11 +6,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
       body: Center(
         child: Container(
           child: Text("welcome to flutter"),
         ),
       ),
+      drawer: Drawer(),
     );
   }
 }
