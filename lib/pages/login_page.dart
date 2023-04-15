23ee842ba83edeafda3cab8a2dfd88421aaa7f59
shayanalvi <shayanalvi6@gmail.com>
2023-04-15 15:25:24 +0500

    update

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text("Login Page"),
+      ),
+    );
+  }
+}
