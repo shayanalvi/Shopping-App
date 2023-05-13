4df2d28322e5f16271a9107afbe1499943ae206c
shayanalvi <shayanalvi6@gmail.com>
2023-05-13 13:38:53 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/pages/login_page.dart';
 import 'pages/home_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
     void main() {
   runApp(MyApp());
@@ -18,7 +17,6 @@ import 'package:google_fonts/google_fonts.dart';
           themeMode: ThemeMode.light,
           theme: ThemeData(
             primarySwatch: Colors.deepPurple,
-            primaryTextTheme: GoogleFonts.latoTextTheme(),
           ),
           darkTheme: ThemeData(
             brightness: Brightness.dark,
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,10 +12,45 @@ class LoginPage extends StatelessWidget {
       child: Column(
         children: [
           Image.asset("assets/images/un_login.png",fit: BoxFit.cover,),
+          SizedBox(
+            height: 20.0,
+          ),
           Text("Welcome",style: TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.bold,
-          ),)
+          ),),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "enter username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "enter password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: (){},
+                )
+              ],
+            ),
+          )
         ],
       )
     );
